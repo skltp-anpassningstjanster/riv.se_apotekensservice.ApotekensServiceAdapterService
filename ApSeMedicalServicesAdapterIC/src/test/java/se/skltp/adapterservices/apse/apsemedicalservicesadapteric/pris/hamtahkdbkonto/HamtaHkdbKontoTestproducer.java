ff94e09043f3431dd5a78d4629df18c9747606a4
hanswiklund <hans.wiklund@kentor.se>
2017-05-18 21:12:09 +0200

    SERVICE-472 Uppdaterat PRIS

--- ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/pris/hamtahkdbkonto/HamtaHkdbKontoTestproducer.java
+++ ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/pris/hamtahkdbkonto/HamtaHkdbKontoTestproducer.java
@@ -1,30 +1,19 @@
 package se.skltp.adapterservices.apse.apsemedicalservicesadapteric.pris.hamtahkdbkonto;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-
-import org.w3c.addressing.v1.AttributedURIType;
-
 import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
-import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp20.ApplicationException;
-import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp20.HamtaHkdbKontoResponderInterface;
-import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp20.SystemException;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp21.ApplicationException;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp21.HamtaHkdbKontoResponderInterface;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbkonto.v1.rivtabp21.SystemException;
 import se.riv.se.apotekensservice.pris.hamtahkdbkontoresponder.v1.HamtaHkdbKontoRequestType;
 import se.riv.se.apotekensservice.pris.hamtahkdbkontoresponder.v1.HamtaHkdbKontoResponseType;
+
 
 public class HamtaHkdbKontoTestproducer implements HamtaHkdbKontoResponderInterface {
 
 	@Override
-	@WebResult(name = "HamtaHkdbKontoResponse", targetNamespace = "urn:riv:se.apotekensservice:pris:HamtaHkdbKontoResponder:1", partName = "parameters")
-	@WebMethod(operationName = "HamtaHkdbKonto", action = "urn:riv:se.apotekensservice:pris:HamtaHkdbKontoResponder:1:HamtaHkdbKonto")
-	public HamtaHkdbKontoResponseType hamtaHkdbKonto(
-			@WebParam(partName = "parameters", name = "HamtaHkdbKonto", targetNamespace = "urn:riv:se.apotekensservice:pris:HamtaHkdbKontoResponder:1") HamtaHkdbKontoRequestType parameters,
-			@WebParam(partName = "LogicalAddress", name = "To", targetNamespace = "http://www.w3.org/2005/08/addressing", header = true) AttributedURIType logicalAddress,
-			@WebParam(partName = "ArgosHeader", name = "ArgosHeader", targetNamespace = "urn:riv:inera.se.apotekensservice:argos:1", header = true) ArgosHeaderType argosHeader)
-			throws SystemException, ApplicationException {
+	public HamtaHkdbKontoResponseType hamtaHkdbKonto(HamtaHkdbKontoRequestType arg0, String arg1, ArgosHeaderType arg2)
+			throws ApplicationException, SystemException {
 		// TODO Auto-generated method stub
-		
 		HamtaHkdbKontoResponseType response = new HamtaHkdbKontoResponseType();
 		return response;
 	}
--- ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/pris/hamtahkdbtransaktionerwebb/HamtaHkdbTransaktionerWebbTestproducer.java
+++ ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/pris/hamtahkdbtransaktionerwebb/HamtaHkdbTransaktionerWebbTestproducer.java
@@ -1,30 +1,20 @@
 package se.skltp.adapterservices.apse.apsemedicalservicesadapteric.pris.hamtahkdbtransaktionerwebb;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-
-import org.w3c.addressing.v1.AttributedURIType;
-
 import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
-import se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp20.HamtaHkdbTransaktionerWebbResponderInterface;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp21.ApplicationException;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp21.HamtaHkdbTransaktionerWebbResponderInterface;
+import se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp21.SystemException;
 import se.riv.se.apotekensservice.pris.hamtahkdbtransaktionerwebbresponder.v1.HamtaHkdbTransaktionerWebbRequestType;
 import se.riv.se.apotekensservice.pris.hamtahkdbtransaktionerwebbresponder.v1.HamtaHkdbTransaktionerWebbResponseType;
+
 
 
 public class HamtaHkdbTransaktionerWebbTestproducer implements HamtaHkdbTransaktionerWebbResponderInterface {
 
 	@Override
-	@WebResult(name = "HamtaHkdbTransaktionerWebbResponse", targetNamespace = "urn:riv:se.apotekensservice:pris:HamtaHkdbTransaktionerWebbResponder:1", partName = "parameters")
-	@WebMethod(operationName = "HamtaHkdbTransaktionerWebb", action = "urn:riv:se.apotekensservice:pris:HamtaHkdbTransaktionerWebbResponder:1:HamtaHkdbTransaktionerWebb")
-	public HamtaHkdbTransaktionerWebbResponseType hamtaHkdbTransaktionerWebb(
-			@WebParam(partName = "parameters", name = "HamtaHkdbTransaktionerWebb", targetNamespace = "urn:riv:se.apotekensservice:pris:HamtaHkdbTransaktionerWebbResponder:1") HamtaHkdbTransaktionerWebbRequestType parameters,
-			@WebParam(partName = "LogicalAddress", name = "To", targetNamespace = "http://www.w3.org/2005/08/addressing", header = true) AttributedURIType logicalAddress,
-			@WebParam(partName = "ArgosHeader", name = "ArgosHeader", targetNamespace = "urn:riv:inera.se.apotekensservice:argos:1", header = true) ArgosHeaderType argosHeader)
-			throws se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp20.SystemException,
-			se.riv.inera.se.apotekensservice.pris.hamtahkdbtransaktionerwebb.v1.rivtabp20.ApplicationException {
+	public HamtaHkdbTransaktionerWebbResponseType hamtaHkdbTransaktionerWebb(HamtaHkdbTransaktionerWebbRequestType arg0,
+			String arg1, ArgosHeaderType arg2) throws SystemException, ApplicationException {
 		// TODO Auto-generated method stub
-		
 		HamtaHkdbTransaktionerWebbResponseType response = new HamtaHkdbTransaktionerWebbResponseType();
 		return response;
 	}
