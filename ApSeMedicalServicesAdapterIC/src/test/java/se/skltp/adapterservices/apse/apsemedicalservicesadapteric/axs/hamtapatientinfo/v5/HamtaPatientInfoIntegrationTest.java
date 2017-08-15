387d83b7f9fb4c34e4f1b28643bcc5a992953066
hanswiklund <hans.wiklund@kentor.se>
2017-08-15 10:01:25 +0200

    buggrättning

---
+++ ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/axs/hamtapatientinfo/v5/HamtaPatientInfoIntegrationTest.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skltp.adapterservices.apse.apsemedicalservicesadapteric.axs.hamtapatientinfo.v5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static se.skltp.adapterservices.apse.apsemedicalservicesadapteric.ApSeMedicalServicesAdapterICMuleServer.getAddress;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soitoolkit.commons.mule.test.AbstractJmsTestUtil;
+import org.soitoolkit.commons.mule.test.ActiveMqJmsTestUtil;
+import org.soitoolkit.commons.mule.test.junit4.AbstractTestCase;
+
+import se.riv.se.apotekensservice.axs.hamtapatientinforesponder.v5.HamtaPatientInfoResponseType;
+import se.skltp.adapterservices.apse.apsemedicalservicesadapteric.lfutskrift.LFUtskriftIntegrationTest;
+
+public class HamtaPatientInfoIntegrationTest extends AbstractTestCase {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(LFUtskriftIntegrationTest.class);
+
+	private static final String DEFAULT_SERVICE_ADDRESS = getAddress("inbound.endpoint.http.apotekensservice.axs.HamtaPatientInfo.v5");
+	private static final String ERROR_LOG_QUEUE = "SOITOOLKIT.LOG.ERROR";
+	private AbstractJmsTestUtil jmsUtil = null;
+
+	public HamtaPatientInfoIntegrationTest() {
+
+		// Only start up Mule once to make the tests run faster...
+		// Set to false if tests interfere with each other when Mule is started
+		// only once.
+		setDisposeContextPerClass(true);
+	}
+
+	@Override
+	protected void doSetUp() throws Exception {
+		super.doSetUp();
+
+		doSetUpJms();
+	}
+
+	private void doSetUpJms() {
+		// TODO: Fix lazy init of JMS connection et al so that we can create
+		// jmsutil in the declaration
+		// (The embedded ActiveMQ queue manager is not yet started by Mule when
+		// jmsutil is delcared...)
+		if (jmsUtil == null)
+			jmsUtil = new ActiveMqJmsTestUtil();
+
+		// Clear queues used for error handling
+		jmsUtil.clearQueues(ERROR_LOG_QUEUE);
+	}
+
+	@Override
+	protected String getConfigResources() {
+		return "soitoolkit-mule-jms-connector-activemq-embedded.xml," + "ApSeMedicalServicesAdapterIC-common.xml,"
+				+ "services/AXS-HamtaPatientInfo-v5-apse-service.xml,"
+				+ "teststub-services/AXS-HamtaPatientInfo-v5-apse-teststub-service.xml";
+	}
+
+	@Test
+	public void testRequestSsnWithCompleteArgosHeader() throws Exception {
+		String ssn = "196308212817";
+		String to = "1234567";
+
+		HamtaPatientInfoResponseType response = new HamtaPatientInfoTestConsumer(DEFAULT_SERVICE_ADDRESS)
+				.requestIncludingCompleteArgosInformation(ssn, to);
+
+		assertNotNull(response);
+		assertTrue(response.isFinnsOrdination());
+	}
+
+	
+	@Test
+	public void testEncodingIsProperThroughIntegration() throws Exception {
+		String ssn = "ÅÄÖ";
+		String to = "1234567";
+
+		HamtaPatientInfoResponseType response = new HamtaPatientInfoTestConsumer(DEFAULT_SERVICE_ADDRESS)
+				.requestIncludingCompleteArgosInformation(ssn, to);
+
+		assertNotNull(response);
+		assertEquals(ssn, response.getDosproducent());
+	}
+
+}
---
+++ ApSeMedicalServicesAdapterIC/src/test/java/se/skltp/adapterservices/apse/apsemedicalservicesadapteric/axs/hamtapatientinfo/v5/HamtaPatientInfoTestProducer.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skltp.adapterservices.apse.apsemedicalservicesadapteric.axs.hamtapatientinfo.v5;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import riv.se_apotekensservice.axs._4.ApoteksinformationResponse;
+import riv.se_apotekensservice.axs._5.PatientInformationResponse;
+import se.riv.inera.se.apotekensservice.argos.v1.ArgosHeaderType;
+import se.riv.se.apotekensservice.axs.hamtapatientinfo.v5.rivtabp21.ApplicationException;
+import se.riv.se.apotekensservice.axs.hamtapatientinfo.v5.rivtabp21.HamtaPatientInfoResponderInterface;
+import se.riv.se.apotekensservice.axs.hamtapatientinfo.v5.rivtabp21.SystemException;
+import se.riv.se.apotekensservice.axs.hamtapatientinforesponder.v5.HamtaPatientInfoRequestType;
+import se.riv.se.apotekensservice.axs.hamtapatientinforesponder.v5.HamtaPatientInfoResponseType;
+
+public class HamtaPatientInfoTestProducer implements HamtaPatientInfoResponderInterface {
+
+	@Override
+	@WebResult(name = "HamtaPatientInfoResponse", targetNamespace = "urn:riv:se.apotekensservice:axs:HamtaPatientInfoResponder:5", partName = "parameters")
+	@WebMethod(operationName = "HamtaPatientInfo", action = "urn:riv:se.apotekensservice:axs:HamtaPatientInfoResponder:5:HamtaPatientInfo")
+	public HamtaPatientInfoResponseType hamtaPatientInfo(
+			@WebParam(partName = "parameters", name = "HamtaPatientInfo", targetNamespace = "urn:riv:se.apotekensservice:axs:HamtaPatientInfoResponder:5") HamtaPatientInfoRequestType parameters,
+			@WebParam(partName = "LogicalAddress", name = "LogicalAddress", targetNamespace = "urn:riv:itintegration:registry:1", header = true) String logicalAddress,
+			@WebParam(partName = "ArgosHeader", name = "ArgosHeader", targetNamespace = "urn:riv:inera.se.apotekensservice:argos:1", header = true) ArgosHeaderType argosHeader)
+			throws SystemException, ApplicationException {
+		// TODO Auto-generated method stub
+		HamtaPatientInfoResponseType response = new HamtaPatientInfoResponseType();
+		String ssn = parameters.getPersonnummer();
+		
+		response.setApotek(createApoteksInformationResponse(ssn));
+		response.setPatientInformation(createPatientInformationResponse());
+		response.setFinnsOrdination(true);
+		response.setDosproducent(ssn);
+		return response;
+	}
+
+    private ApoteksinformationResponse createApoteksInformationResponse(String ssn) {
+	ApoteksinformationResponse apoteksinformationResponse = new ApoteksinformationResponse();
+	apoteksinformationResponse.setAktorsnamn("Aktörsnamn");
+	return null;
+    }
+
+    private PatientInformationResponse createPatientInformationResponse() {
+	PatientInformationResponse informationResponse = new PatientInformationResponse();
+	informationResponse.setStatusKod("OK");
+	return null;
+    }
+
+}
